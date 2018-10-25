82912315619cc0b204aa062dcae6194ff87afd4d
Doug Enas <douglassenas@gmail.com>
2018-10-25 11:53:51 -0400

    passed url parse into MongoClient.connect

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'myproject';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
